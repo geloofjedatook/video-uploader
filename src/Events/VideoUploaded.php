fd79b266853c0035f25492f613b48c87bec64b43
geloofjedatook <podcast@geloofjedatook.nl>
2025-10-29 16:35:22 +0100

    Update VideoUploaded.php

--- src/Events/VideoUploaded.php
+++ src/Events/VideoUploaded.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace Plugins\GeloofJeDatOok\VideoUploader\Src\Events;
+
+use Castopod\Models\Episode;
+
+class VideoUploaded
+{
+    /**
+     * The associated episode instance.
+     */
+    public Episode $episode;
+
+    /**
+     * Absolute path to the uploaded video file.
+     */
+    public string $filePath;
+
+    public function __construct(Episode $episode, string $filePath)
+    {
+        $this->episode = $episode;
+        $this->filePath = $filePath;
+    }
+}
